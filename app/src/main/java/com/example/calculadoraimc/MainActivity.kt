7c8c2d658ee2fccfd17da4a18ba5377aabd78789
RubenNeo <ruben.lazaronp@gmail.com>
2024-06-04 14:01:43 +0200

    1 pasos en ANDROID

--- app/src/main/java/com/example/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.calculadoraimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var WelcomeTextView: TextView
+    lateinit var ClickButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        WelcomeTextView = findViewById(R.id.WelcomeTextView)
+        ClickButton = findViewById(R.id.ClickButton)
+
+
+        WelcomeTextView.text = "CalculadoraIMC"
+
+        ClickButton.setOnClickListener {
+            WelcomeTextView.text = "Texto del Click del Boton"
+
+        }
     }
 }
